f8bedb6e4af86af494ef9069f56801d20bdb3ca1
Jatin <githubgit.repositories@gmail.com>
2023-07-28 16:18:36 +0530

    usecase: duration for task added

--- components/StopWatch.tsx
+++ components/StopWatch.tsx
@@ -3,26 +3,42 @@
 import React, { useState, useEffect } from "react";
 
 export default function StopWatch() {
+  const endTime = "14:50";
   const [value, setValue] = useState(50);
-
-  // //clock logic
-  // const [time, setTime] = useState(new Date());
-
-  // useEffect(() => {
-  //   // Update the time every second
-  //   const interval = setInterval(() => {
-  //     setTime(new Date());
-  //   }, 1000);
-
-  //   // Clean up the interval when the component unmounts
-  //   return () => clearInterval(interval);
-  // }, []);
-
-  // // Format the time as HH:MM:SS
-  // const formattedTime = time.toLocaleTimeString();
-
   const [seconds, setSeconds] = useState(0);
   const [isActive, setIsActive] = useState(false);
+
+  const handleStartStop = () => {
+    setIsActive((prevIsActive) => !prevIsActive);
+  };
+
+  const handleReset = () => {
+    setIsActive(false);
+    setSeconds(0);
+  };
+
+  const formatTime = (seconds: number): string => {
+    const hours = Math.floor(seconds / 3600);
+    const minutes = Math.floor((seconds % 3600) / 60);
+    const remainingSeconds = Math.floor(seconds % 60);
+
+    const formattedHours = String(hours).padStart(2, "0");
+    const formattedMinutes = String(minutes).padStart(2, "0");
+    const formattedSeconds = String(remainingSeconds).padStart(2, "0");
+
+    return `${formattedHours}:${formattedMinutes}:${formattedSeconds}`;
+  };
+
+  useEffect(() => {
+    if (endTime) {
+      const [hours, minutes] = endTime.split(":").map(Number);
+      const currentDate = new Date(Date.now());
+      currentDate.setHours(hours);
+      currentDate.setMinutes(minutes);
+      console.log(currentDate.getTime() - Date.now());
+      setSeconds((currentDate.getTime() - Date.now()) / 1000);
+    }
+  }, []);
 
   useEffect(() => {
     let interval: NodeJS.Timeout;
@@ -38,44 +54,54 @@ export default function StopWatch() {
     };
   }, [isActive]);
 
-  const handleStartStop = () => {
-    setIsActive((prevIsActive) => !prevIsActive);
-  };
-
-  const handleReset = () => {
-    setIsActive(false);
-    setSeconds(0);
-  };
-
-  const formatTime = (seconds: number): string => {
-    const hours = Math.floor(seconds / 3600);
-    const minutes = Math.floor((seconds % 3600) / 60);
-    const remainingSeconds = seconds % 60;
-
-    const formattedHours = String(hours).padStart(2, "0");
-    const formattedMinutes = String(minutes).padStart(2, "0");
-    const formattedSeconds = String(remainingSeconds).padStart(2, "0");
-
-    return `${formattedHours}:${formattedMinutes}:${formattedSeconds}`;
-  };
   return (
-    <div className="relative grid justify-center content-center">
-      <div
-        className={`pie absolute shadow-[0_0px_18px_0px_#d5b7fc] ${
-          isActive ? "animate-pulse" : ""
-        }`}
-        style={{
-          backgroundImage: `conic-gradient(#d5b7fc ${value / 4}%, #a267ed ${
-            value / 2
-          }%, #b983fb ${value / 4 + value / 2}%, #d5b7fc ${value}%, #2d3035 0)`,
-        }}
-      ></div>
-      <div
-        className="pie2 absolute grid items-center justify-center bg-[#1d1f25] cursor-pointer"
-        onClick={handleStartStop}
-      >
-        {formatTime(seconds)}
+    <>
+      <div className="relative grid justify-center content-center h-64">
+        <div
+          className={`pie absolute shadow-[0_0px_18px_0px_#d5b7fc] ${
+            isActive ? "animate-pulse" : ""
+          }`}
+          style={{
+            backgroundImage: `conic-gradient(#d5b7fc ${value / 4}%, #a267ed ${
+              value / 2
+            }%, #b983fb ${
+              value / 4 + value / 2
+            }%, #d5b7fc ${value}%, #2d3035 0)`,
+          }}
+        ></div>
+        <div
+          className="pie2 absolute grid items-center justify-center bg-[#1d1f25] cursor-pointer"
+          onClick={handleStartStop}
+        >
+          {formatTime(seconds)}
+        </div>
       </div>
-    </div>
+      {/* <input type="date" /> */}
+      <section>
+        <form action="" className="flex flex-col w-1/4 gap-2">
+          <div className="flex">
+            <input type="text" placeholder="Task Name" />
+            <button>+</button>
+          </div>
+          <div>
+            <button>Flexible Hours</button>
+            <button>Strict Hours</button>
+          </div>
+          <select name="Priority" id="">
+            <option value="p1">1</option>
+            <option value="p1">1</option>
+            <option value="p1">1</option>
+            <option value="p1">1</option>
+          </select>
+          <input type="date" />
+          <select name="Repeat" id="">
+            <option value="p1">1</option>
+            <option value="p1">1</option>
+            <option value="p1">1</option>
+            <option value="p1">1</option>
+          </select>
+        </form>
+      </section>
+    </>
   );
 }
