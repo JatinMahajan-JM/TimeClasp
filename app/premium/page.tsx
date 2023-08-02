25b17ad3ec63f08cf4ee21fe1aef6e4a7a428a0d
Jatin <githubgit.repositories@gmail.com>
2023-08-02 20:21:51 +0530

    Trying to add premium

--- app/premium/page.tsx
+++ app/premium/page.tsx
@@ -1,3 +1,62 @@
+"use client";
+
+import getStripe from "@/utils/get-stripejs";
+import { useState } from "react";
+
 export default function Premium() {
-  return <h1>Premium</h1>;
+  const [loading, setLoading] = useState(false);
+  const handleSubmit = async () => {
+    // e.preventDefault();
+    setLoading(true);
+    // Create a Checkout Session.
+    const response = await fetchPostJSON("/api/checkout_sessions", {
+      amount: 4,
+    });
+
+    if (response.statusCode === 500) {
+      console.error(response.message);
+      return;
+    }
+
+    // Redirect to Checkout.
+    const stripe = await getStripe();
+    const { error } = await stripe!.redirectToCheckout({
+      // Make the id field from the Checkout Session creation API response
+      // available to this file, so you can provide it as parameter here
+      // instead of the {{CHECKOUT_SESSION_ID}} placeholder.
+      sessionId: response.id,
+    });
+    // If `redirectToCheckout` fails due to a browser or network
+    // error, display the localized error message to your customer
+    // using `error.message`.
+    console.warn(error.message);
+    setLoading(false);
+  };
+  return (
+    <button onClick={handleSubmit} disabled={loading}>
+      Pay $4
+    </button>
+  );
+}
+
+export async function fetchPostJSON(url: string, data?: {}) {
+  try {
+    // Default options are marked with *
+    const response = await fetch(url, {
+      method: "POST", // *GET, POST, PUT, DELETE, etc.
+      mode: "cors", // no-cors, *cors, same-origin
+      cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+      credentials: "same-origin", // include, *same-origin, omit
+      headers: {
+        "Content-Type": "application/json",
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      redirect: "follow", // manual, *follow, error
+      referrerPolicy: "no-referrer", // no-referrer, *client
+      body: JSON.stringify(data || {}), // body data type must match "Content-Type" header
+    });
+    return await response.json(); // parses JSON response into native JavaScript objects
+  } catch (err: any) {
+    throw new Error(err.message);
+  }
 }
