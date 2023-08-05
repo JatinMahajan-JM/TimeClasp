3ff4e3da40374b103364dccb0e07b7f34c697978
Jatin <githubgit.repositories@gmail.com>
2023-08-05 16:21:19 +0530

    fixed the timer againe=

--- components/AllTasks.tsx
+++ components/AllTasks.tsx
@@ -1,11 +1,64 @@
+"use client";
+
+import { useContext } from "react";
 import AllTasksClient from "./AllTasksClient";
+import { Ctx } from "./TasksMain";
+import { updateTaskData } from "@/api/tasksApi";
 
-async function getAllTasks() {
-  const res = await fetch("http://localhost:3000/api/task");
-  return await res.json();
-}
+export default function AllTasks() {
+  const { data, selectedTask, isActive, seconds, dispatch } = useContext(Ctx);
 
-export default async function AllTasks() {
-  const allTasks = await getAllTasks();
-  return <AllTasksClient data={allTasks} />;
+  const dataModification = (updateData: { [key: string]: any }) => {
+    const modifiedIndex = data.findIndex(
+      (item) => item._id === selectedTask?._id
+    );
+    const newDataState = [...data]; // Create a copy of the state array
+    newDataState[modifiedIndex] = {
+      ...newDataState[modifiedIndex],
+      ...updateData,
+    };
+    return newDataState;
+  };
+
+  const handleTaskClick = (id: string) => {
+    if (selectedTask?._id === id) return;
+    if (isActive) {
+      // setIsActive(false);
+      dispatch({ type: "isActive", payload: { active: false } });
+      let updateData = {
+        timeWorked: seconds,
+        timerStartTime: Date.now(),
+        timerEnded: true,
+      };
+      // const res = await fetch("/api/task", {
+      //   method: "PUT",
+      //   headers: {
+      //     "Content-Type": "application/json",
+      //   },
+      //   body: JSON.stringify({
+      //     _id: selectedTask?._id,
+      //     data: updateData,
+      //   }),
+      // });
+      updateTaskData(updateData);
+      // console.log(await res.json());
+      // const updatedRes = await fetch("/api/task");
+      // const updatedData = await updatedRes.json();
+      // setDataState(updatedData);
+      dispatch({
+        type: "dataState",
+        payload: { data: dataModification(updateData) },
+      });
+    }
+    const selectedItem = data.find((task) => task._id === id);
+    // setSelectedTask(selectedItem);
+    // setSeconds(selectedItem?.timeWorked);
+    // console.log(selectedItem);
+    dispatch({ type: "selectedTask", payload: { selectedTask: selectedItem } });
+    dispatch({
+      type: "seconds",
+      payload: { seconds: selectedItem?.timeWorked },
+    });
+  };
+  return <AllTasksClient data={data} handleClick={handleTaskClick} />;
 }
