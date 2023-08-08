c5bb60a0b6a07cf5a2de52f05fb2708a00b07123
Jatin <githubgit.repositories@gmail.com>
2023-08-08 21:51:54 +0530

    creating dashboard

--- app/statistics/page.tsx
+++ app/statistics/page.tsx
@@ -1,9 +1,24 @@
 import BarChart from "@/components/BarChart";
+import TimeStats from "@/components/TimeStats";
 
-export default function Statistics() {
+async function getUpdatedData() {
+  const res = await fetch("http://localhost:3000/api/task", {
+    next: { revalidate: 10000 },
+  });
+  return await res.json();
+}
+
+export default async function Statistics() {
+  const data = await getUpdatedData();
   return (
     <>
-      <BarChart />
+      <div className="grid grid-cols-[1fr_350px]">
+        <div>
+          <TimeStats data={data} />
+          <BarChart />
+        </div>
+        <div></div>
+      </div>
     </>
   );
 }
