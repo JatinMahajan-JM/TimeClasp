0b331756229e9ea15521f8eecfc684acd043db94
Jatin <githubgit.repositories@gmail.com>
2023-08-11 16:22:03 +0530

    added [...nextauth].ts file for authoption

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -3,46 +3,48 @@ import GoogleProvider from 'next-auth/providers/google';
 
 import UserModel from '@/models/userModel';
 import { connectToDatabase } from '@/dbConfig/dbConfig';
+import Nextauth from './[...nextauth]';
 
-const handler = NextAuth({
-    providers: [
-        GoogleProvider({
-            clientId: process.env.GOOGLE_ID!,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-        })
-    ],
-    callbacks: {
-        async session({ session }: { session: any }) {
-            // store the user id from MongoDB to session
-            // console.log(session)
-            const sessionUser = await UserModel.findOne({ email: session.user?.email });
-            session.user.id = sessionUser._id.toString();
+// const handler = NextAuth({
+//     providers: [
+//         GoogleProvider({
+//             clientId: process.env.GOOGLE_ID!,
+//             clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+//         })
+//     ],
+//     callbacks: {
+//         async session({ session }: { session: any }) {
+//             // store the user id from MongoDB to session
+//             // console.log(session)
+//             const sessionUser = await UserModel.findOne({ email: session.user?.email });
+//             session.user.id = sessionUser._id.toString();
 
-            return session;
-        },
-        async signIn({ account, profile, user, credentials }) {
-            try {
-                await connectToDatabase();
+//             return session;
+//         },
+//         async signIn({ account, profile, user, credentials }) {
+//             try {
+//                 await connectToDatabase();
 
-                // check if user already exists
-                const userExists = await UserModel.findOne({ email: profile?.email });
+//                 // check if user already exists
+//                 const userExists = await UserModel.findOne({ email: profile?.email });
 
-                // if not, create a new document and save user in MongoDB
-                if (!userExists) {
-                    await UserModel.create({
-                        email: profile?.email,
-                        username: profile?.name?.replace(" ", "").toLowerCase(),
-                        // image: profile.picture,
-                    });
-                }
+//                 // if not, create a new document and save user in MongoDB
+//                 if (!userExists) {
+//                     await UserModel.create({
+//                         email: profile?.email,
+//                         username: profile?.name?.replace(" ", "").toLowerCase(),
+//                         // image: profile.picture,
+//                     });
+//                 }
 
-                return true
-            } catch (error: any) {
-                console.log("Error checking if user exists: ", error.message);
-                return false
-            }
-        },
-    }
-})
+//                 return true
+//             } catch (error: any) {
+//                 console.log("Error checking if user exists: ", error.message);
+//                 return false
+//             }
+//         },
+//     }
+// })
+const handler = Nextauth;
 
 export { handler as GET, handler as POST }
