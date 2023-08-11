3ad75ff2abcd0fed307d9a816a46739854f5e13c
Jatin <githubgit.repositories@gmail.com>
2023-08-11 16:22:52 +0530

    user field added every document

--- app/api/time/route.ts
+++ app/api/time/route.ts
@@ -1,16 +1,22 @@
 import { connectToDatabase } from "@/dbConfig/dbConfig";
 import timeModel from "@/models/timeModel";
+import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
+import { authOptions } from "../auth/[...nextauth]/[...nextauth]";
 
 connectToDatabase()
 
 export async function GET() {
+    const session: any = await getServerSession(authOptions);
     const thirtyDaysAgo = new Date();
     thirtyDaysAgo.setDate(thirtyDaysAgo.getDate() - 30);
     try {
+        let allTasks: any;
+        if (session) {
+            allTasks = await timeModel.find({ userId: session.user.id, date: { $gte: thirtyDaysAgo.toISOString().slice(0, 10) } }).populate("tasks.taskId");
+        } else allTasks = []
         // const allTasks = await timeModel.find({ date: new Date().toISOString().slice(0, 10) }).populate("tasks.taskId");
-        const allTasks = await timeModel.find({ date: { $gte: thirtyDaysAgo.toISOString().slice(0, 10) } }).populate("tasks.taskId");
-        console.log(allTasks)
+        console.log(session, allTasks, "this is the stats route")
         return NextResponse.json(allTasks)
     } catch (err: any) {
         console.log(err)
@@ -18,8 +24,9 @@ export async function GET() {
 }
 
 export async function PUT(request: NextRequest) {
+    const session: any = await getServerSession(authOptions);
     const { _id, timeWorkedToday, date } = await request.json();
-    console.log(_id, timeWorkedToday)
+    console.log(_id, timeWorkedToday, session, "this is the put request")
     try {
         // const a = await timeModel.updateOne({ _id }, { $set: data });
         // find task using taskId and then update that task 
@@ -50,13 +57,15 @@ export async function PUT(request: NextRequest) {
         if (a) {
             const taskToUpdate = a.tasks.find((task: any) => task.taskId.equals(_id));
             if (taskToUpdate) {
+                a.totalTimeWorked = a.totalTimeWorked - taskToUpdate.timeWorkedToday + timeWorkedToday;
                 taskToUpdate.timeWorkedToday = timeWorkedToday;
             }
         } else {
             // Document doesn't exist, create a new one and add the task
             a = new timeModel({
                 date: date.slice(0, 10),
-                tasks: [{ taskId: _id, timeWorkedToday }]
+                tasks: [{ taskId: _id, timeWorkedToday }],
+                userId: session.user.id
             });
         }
 
