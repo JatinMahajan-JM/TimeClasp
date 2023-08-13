7a3fb06d1b770e4d03ab6932000c4e1060c3c772
Jatin <githubgit.repositories@gmail.com>
2023-08-13 19:24:46 +0530

    initialized deleteById

---
+++ app/api/task/deleteById/route.ts
@@ -0,0 +1,33 @@
+import taskModel from "@/models/taskModel";
+import timeModel from "@/models/timeModel";
+import UserModel from "@/models/userModel";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function DELETE(request: NextRequest) {
+    const { _id } = await request.json();
+    try {
+        console.log(_id, "ToDeleteId")
+        const taskToDelete = await taskModel.findOne({ _id })
+        if (!taskToDelete) return NextResponse.json({ message: "Task not found" }, { status: 404 });
+
+        const timeWorked = taskToDelete.timeWorked ?? 0;
+
+        // Delete task references from userSchema
+        await UserModel.updateMany(
+            { taskList: _id },
+            { $pull: { taskList: _id } }
+        );
+
+        // Delete task references from timeSchema
+        await timeModel.updateMany(
+            { 'tasks.taskId': _id },
+            { $pull: { tasks: { taskId: _id } }, $inc: { totalTimeWorked: -timeWorked } }
+        );
+
+        // Delete the actual task
+        await taskModel.deleteOne({ _id });
+        return NextResponse.json({ message: "Task deleted successfully" }, { status: 202 })
+    } catch (err: any) {
+        return NextResponse.json({ error: err.message }, { status: 500 })
+    }
+}
