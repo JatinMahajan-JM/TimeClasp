89c65bfffe67cd9163091b3de65162f1f68dc2ab
Jatin <githubgit.repositories@gmail.com>
2023-08-15 17:53:34 +0530

    logs for bugs fix

--- app/api/task/[id]/route.ts
+++ app/api/task/[id]/route.ts
@@ -6,15 +6,16 @@ import { NextResponse } from "next/server";
 export async function GET(request: Request, { params }: { params: { id: string } }) {
     try {
         await connectToDatabase();
-        // console.log(params.id, "the id is here")
+        console.log(params.id, "Param Id")
         let allTasks: any = [];
         if (params.id) {
             allTasks = await UserModel.findOne({ _id: params.id }).populate("taskList");
             allTasks = allTasks.taskList;
         }
-        // console.log(allTasks, "allTasks")
+        console.log(allTasks, "TaskList")
         return NextResponse.json({ allTasks }, { status: 200 })
     } catch (err: any) {
+        console.log(err);
         return NextResponse.json({ err }, { status: 404 })
     }
 }
