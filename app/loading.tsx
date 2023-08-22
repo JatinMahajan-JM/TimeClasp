63583f1e849d522b5d0b279ad9421fdd3e27eeef
Jatin <githubgit.repositories@gmail.com>
2023-08-22 21:02:56 +0530

    suspense introduced

---
+++ app/loading.tsx
@@ -0,0 +1,10 @@
+import LoadingII from "@/components/ui/LoadingII";
+import LoadingSpinner from "@/components/ui/loadingSpinner";
+
+export default function Loading() {
+  return (
+    <div className="h-screen grid items-center relative">
+      <LoadingII />
+    </div>
+  );
+}
