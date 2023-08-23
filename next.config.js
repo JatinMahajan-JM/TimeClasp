9947480ba45c68f09565c8a5aa95bdd73f0e4909
Jatin <githubgit.repositories@gmail.com>
2023-08-23 16:56:07 +0530

    config changed

--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    swcMinify: true,
+    experimental: {
+        appDir: true,
+        serverComponentsExternalPackages: ["mongoose"],
+        //     fastRefresh: true,
+        //     concurrentFeatures: true,
+    },
+    productionBrowserSourceMaps: false, // Disable source maps in development
+    optimizeFonts: false, // Disable font optimization
+    webpack(config) {
+        config.experiments = { ...config.experiments, topLevelAwait: true };
+        return config;
+    }
+    // minify: false, // Disable minification
+}
 
 module.exports = nextConfig
